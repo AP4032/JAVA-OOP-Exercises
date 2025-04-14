757eb7fa39a22e0f595b5340c5adacf972f4f22a
alireza <alireza10abasi@gmail.com>
2025-04-14 11:14:19 +0330

    updated project.code 02

--- 2.WordGuessGame/Game.java
+++ 2.WordGuessGame/Game.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
@@ -54,14 +56,54 @@ public class Game {
             }
             if (!found) {
                 System.out.println("wrong guess!");
+                attempts--;
             }
-            attempts--;
             if (String.valueOf(guessedWord).contentEquals(chosenWord)) {
                 System.out.println("Congratulations," + player.getName() + "! You guessed the word " + chosenWord);
                 break;
             } else if (attempts == 0) {
                 System.out.println("you didnt guess the word! it was " + chosenWord);
                 break;
+            }
+        }
+        System.out.println("game over!");
+    }
+
+    public void multiPlay(ArrayList<Player> players) {
+        Scanner wordScanner = new Scanner(System.in);
+        closeThis:
+        while (attempts > 0) {
+            for (Player player : players) {
+                System.out.println("attempts = " + attempts + "\t,\t word length = " + chosenWord.length());
+                System.out.println("word: " + String.valueOf(guessedWord));
+                System.out.println(player.getName() + ", enter a letter to guess: ");
+                char guess = wordScanner.next().charAt(0);
+                boolean found = false;
+
+                if (new String(guessedWord).contains(String.valueOf(guess))) {
+                    System.out.println("You've already guessed this letter!");
+                    continue;
+                }
+
+                for (int i = 0; i < chosenWord.length(); i++) {
+                    if (chosenWord.charAt(i) == guess) {
+                        found = true;
+                        guessedWord[i] = guess;
+                    }
+                }
+                if (!found) {
+                    System.out.println(player.getName() + ", you guessed wrong!");
+                    attempts--;
+                }else System.out.println(player.getName() + ", you guessed right!");
+
+
+                if (String.valueOf(guessedWord).contentEquals(chosenWord)) {
+                    System.out.println(player.getName() + ", Congratulations! You guessed the word " + chosenWord);
+                    break closeThis;
+                } else if (attempts == 0) {
+                    System.out.println("you didnt guess the word! it was " + chosenWord);
+                    break closeThis;
+                }
             }
         }
         System.out.println("game over!");
