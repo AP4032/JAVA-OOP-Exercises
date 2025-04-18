036b4294a7316e57c153ba83d43e09f3c2792942
ITech <elahehvaseqi@gmail.com>
2025-04-18 18:01:27 +0330

    Completed 8

---
+++ 8.OnlineShoppingSystem/OnlineShoppingSystem/untitled/src/main/java/org/example/Customer.java
@@ -0,0 +1,72 @@
+package org.example;
+public class Customer {
+    private String username;
+    private String phoneNumber;
+    private double walletBalance;
+    private ShoppingCart shoppingCart;
+
+    public Customer(String username, String phoneNumber, double walletBalance) {
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.walletBalance = walletBalance;
+        this.shoppingCart = new ShoppingCart();
+    }
+
+    public void addToCart(Product product, int quantity) {
+        shoppingCart.addItem(product, quantity);
+    }
+
+    public void viewCart() {
+        shoppingCart.viewCart();
+    }
+
+    public void placeOrder() {
+        double total = shoppingCart.calculateTotal();
+        if (walletBalance >= total) {
+            walletBalance -= total;
+            shoppingCart.checkout();
+            System.out.println("Order placed. Remaining wallet balance: $" + walletBalance);
+        } else {
+            System.out.println("Not enough balance in wallet. Needed: $" + total);
+        }
+    }
+
+    public void addFunds(double amount) {
+        if (amount > 0) {
+            walletBalance += amount;
+            System.out.println("$" + amount + " added to wallet. New balance: $" + walletBalance);
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public double getWalletBalance() {
+        return walletBalance;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setWalletBalance(double walletBalance) {
+        this.walletBalance = walletBalance;
+    }
+}
+
+
---
+++ 8.OnlineShoppingSystem/OnlineShoppingSystem/untitled/src/main/java/org/example/UserManager.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.HashMap;
+
+public class UserManager {
+    private HashMap<String, String> users;
+
+    public UserManager() {
+        users = new HashMap<>();
+    }
+    public boolean registerUser(String username, String password) {
+        if (users.containsKey(username)) {
+            System.out.println("Username is already taken");
+            return false;
+        }
+        users.put(username, password);
+        System.out.println("Username is registered successfully");
+        return true;
+    }
+    public boolean loginUser(String username, String password) {
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            System.out.println(" Login successful!");
+            return true;
+        } else {
+            System.out.println(" Incorrect username or password.");
+            return false;
+        }
+    }
+}
