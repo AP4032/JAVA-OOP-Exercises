a2f042ff2ad763da5ecdbae343743d7ea22dedf6
Sarina <sarina.shakeri0806@gmail.com>
2025-04-22 19:12:19 +0330

    JAVA-OOP-Exercises (2, 3, 5, 7) completed. (final)

---
+++ 3.SportsCourtBooking/MyJava3/Booking.java
@@ -0,0 +1,26 @@
+package MyJava3;
+
+
+public class Booking {
+    private final Player player;
+    private final Schedule schedule;
+    private final double amount;
+
+    public Booking(Player player, Schedule Schedule, double hourlyRate) {
+        this.player = player;
+        this.schedule = Schedule;
+        this.amount = hourlyRate;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return player.getName() + " reserved from " + schedule + " ($" + amount + ")";
+    }
+}
