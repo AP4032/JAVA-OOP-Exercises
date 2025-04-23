b4280e0994061414f5b7b6da556b087e06a58d53
Raya-Mehran <sunshineatnoon1952@gmail.com>
2025-04-23 14:32:34 +0430

    banking system done

---
+++ 7.SimpleBankingSystem/BankingSystem/src/main/java/org/example/Bank.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Bank {
+    private Customer[] customers;
+    private int customerCount;
+
+    public Bank(int maxCustomers) {
+        this.customers = new Customer[maxCustomers];
+        this.customerCount = 0;
+    }
+    public void addCustomer(Customer customer) {
+        for (int i = 0; i < customerCount; i++) {
+            if (customer.equals(customers[i])) {
+                System.out.println("Customer already exists.");
+                return;
+            }
+        }
+        if (customerCount < customers.length) {
+            customers[customerCount] = customer;
+            customerCount++;
+            System.out.println("Customer " + customer.getName() + " added successfully.");
+        } else {
+            System.out.println("Bank has reached its maximum number of customers.");
+        }
+    }
+    public Customer getCustomer(String name) {
+        for (int i = 0; i < customerCount; i++) {
+            if (customers[i].getName().equals(name)) {
+                return customers[i];
+            }
+        }
+        return null;
+    }
+}
